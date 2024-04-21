0f690a317239278ac84b27c3598a9210840c2c65
Renato Alves <rena02to@gmail.com>
2024-04-21 18:45:26 -0300

    adicionando algumas mudanças na personalização

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -4,11 +4,11 @@ import { useTranslation } from 'react-i18next';
 export default function Start(){
     const { t } = useTranslation();
     const cards = [
-        { key: 1, value: <p className={style.seo}>{t('start.seo')}</p> },
-        { key: 2, value: <p className={style.responsive}>{t('start.responsive')}</p> },
-        { key: 3, value: <p className={style.innovation}>{t('start.innovation')}</p> },
-        { key: 4, value: <p className={style.next}>Next.js</p> },
-        { key: 5, value: <p className={style.uix}>UI & UX</p> },
+        { value: <p className={style.seo}>{t('start.seo')}</p> },
+        { value: <p className={style.responsive}>{t('start.responsive')}</p> },
+        { value: <p className={style.innovation}>{t('start.innovation')}</p> },
+        { value: <p className={style.next}>Next.js</p> },
+        { value: <p className={style.uix}>UI & UX</p> },
     ];
 
     return(
@@ -16,7 +16,7 @@ export default function Start(){
             <h1 className={style.name}>Renato Alves</h1>
             <div className={style.text}>
                 <h1>{t('start.dev.1')}<br/>{t('start.dev.2')}</h1>
-                <p className={style.portfolio}>PORTFOLIO</p>
+                <h2 className={style.portfolio}>PORTFOLIO</h2>
                 <p className={style.phrase}>{t("start.phrase")}</p>
                 {cards.map((card) => (
                     <>{card.value}</>
--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -3,7 +3,7 @@ import style from '@/styles/summary.module.scss';
 export default function Summary(){
     return(
         <section className={style.summary} id='summary'>
-            
+            <h2></h2>
         </section>
     );
 }
