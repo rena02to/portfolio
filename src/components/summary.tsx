972eb71cea759b4fb1c5fc48bb87f923a91c13cf
Renato Alves <rena02to@gmail.com>
2024-04-21 23:21:20 -0300

    pequenas alterações no código

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -3,7 +3,6 @@ import style from '@/styles/summary.module.scss';
 export default function Summary(){
     return(
         <section className={style.summary} id='summary'>
-            <h2></h2>
         </section>
     );
 }
