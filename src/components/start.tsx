8d583e88c98753f001394708a1af0b81e7174142
Renato Alves <rena02to@gmail.com>
2024-04-23 23:34:08 -0300

    adicionando o icone de site externo nos links da seção inicial

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -2,7 +2,7 @@ import { RiLinkedinLine } from 'react-icons/ri';
 import style from './../styles/start.module.scss';
 import { useTranslation } from 'react-i18next';
 import { SlSocialGithub } from 'react-icons/sl';
-import { HiOutlineDocumentText } from 'react-icons/hi';
+import { HiOutlineDocumentText, HiOutlineExternalLink } from 'react-icons/hi';
 
 export default function Start(){
     const { t } = useTranslation();
@@ -27,16 +27,19 @@ export default function Start(){
             </div>
             <div className={style.links}>
                 <a href='https://linkedin.com/in/renatosalves' target="_blank" className={style.linkedin}>
-                    <RiLinkedinLine/>
+                    <RiLinkedinLine className={style.icon}/>
                     <p>Linkedin</p>
+                    <HiOutlineExternalLink className={style.external}/>
                 </a>
                 <a href='https://github.com/rena02to' target="_blank" className={style.github}>
-                    <SlSocialGithub/>
+                    <SlSocialGithub className={style.icon}/>
                     <p>GitHub</p>
+                    <HiOutlineExternalLink className={style.external}/>
                 </a>
                 <a href='https://www.canva.com/design/DAFtx-3KHsg/js_-0M_Nu7iDI5Rw_qaQkA/edit?utm_content=DAFtx-3KHsg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton' target="_blank" className={style.curriculum}>
-                    <HiOutlineDocumentText/>
+                    <HiOutlineDocumentText className={style.icon}/>
                     <p>CV</p>
+                    <HiOutlineExternalLink className={style.external}/>
                 </a>
             </div>
         </section>
