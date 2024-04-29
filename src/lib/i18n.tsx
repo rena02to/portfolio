5bad327bd0e7b9d7c78a9a27dbba1aa45cf29acd
Renato Alves <rena02to@gmail.com>
2024-04-29 20:45:28 -0300

    removendo linhas de código desnecessárias

--- src/lib/i18n.tsx
+++ src/lib/i18n.tsx
@@ -3,7 +3,6 @@ import { initReactI18next } from 'react-i18next';
 import ptTranslation from './languages/pt.json';
 import enTranslation from './languages/en.json';
 import esTranslation from './languages/es.json';
-import { useContexts } from '@/context/context';
 
 i18n.use(initReactI18next).init({
     resources: {
