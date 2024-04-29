e32c9f647050d8140ac8249808ad9ec750c116ea
Renato Alves <rena02to@gmail.com>
2024-04-28 22:11:18 -0300

    alterando o formato da seção de resumo

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -1,27 +1,33 @@
 import style from '@/styles/summary.module.scss';
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
+import { BiSolidMobileVibration } from 'react-icons/bi';
+import { FaCss3Alt, FaGitAlt, FaGithub, FaHtml5 } from 'react-icons/fa6';
+import { MdTouchApp } from 'react-icons/md';
+import { PiUserFocus } from 'react-icons/pi';
+import { RiJavascriptFill } from 'react-icons/ri';
+import { SiNextdotjs, SiSass, SiSearxng } from 'react-icons/si';
 
 export default function Summary(){
     const { t } = useTranslation();
     const skills = [
-        { key: 1, value: "HTML + CSS" },
-        { key: 2, value: "Next.js" },
-        { key: 3, value: "JavaScript" },
-        { key: 4, value: "Sass" },
-        { key: 5, value: "SEO" },
-        { key: 6, value: "UI + UX" },
-        { key: 7, value: t('summary.skills.responsive') },
-        { key: 8, value: "Git + GitHub" },
+        { key: 1, value: "HTML", icon: <FaHtml5/> },
+        { key: 2, value: "CSS", icon: <FaCss3Alt/> },
+        { key: 3, value: "Next.js", icon: <SiNextdotjs/> },
+        { key: 4, value: "JavaScript", icon: <RiJavascriptFill/> },
+        { key: 5, value: "Sass", icon: <SiSass/> },
+        { key: 6, value: "SEO", icon: <SiSearxng/> },
+        { key: 7, value: "UI", icon: <MdTouchApp/> },
+        { key: 8, value: "UX", icon: <PiUserFocus/> },
+        { key: 9, value: t('summary.skills.responsive'), icon: <BiSolidMobileVibration/> },
+        { key: 10, value: "Git", icon: <FaGitAlt/> },
+        { key: 11, value: "GitHub", icon: <FaGithub/> }
     ];
-    const experiences = [
-        { key: null, title: null, value: null },
-        { key: null, title: null, value: null },
-        { key: null, title: null, value: null },
-        { key: null, title: null, value: null },
-        { key: null, title: null, value: null },
-        { key: null, title: null, value: null },
-        { key: null, title: null, value: null },
+    const desc = [
+        { key: 1, title: t('summary.skills.desc.front.title'), value: t('summary.skills.desc.front.desc') },
+        { key: 2, title: t('summary.skills.desc.seo.title'), value: t('summary.skills.desc.seo.desc') },
+        { key: 3, title: t('summary.skills.desc.uix.title'), value: t('summary.skills.desc.uix.desc') },
+        { key: 4, title: t('summary.skills.desc.responsive.title'), value: t('summary.skills.desc.responsive.desc') },
     ];
 
     return(
@@ -45,9 +51,15 @@ export default function Summary(){
                         <h2>{t('summary.skills.title')}</h2>
                         <ul>
                             {skills.map((skill) => (
-                                <li key={skill.key}>{skill.value}</li>
+                                <li key={skill.key}>{skill.icon}{skill.value}</li>
                             ))}
                         </ul>
+                        {desc.map((describe) => (
+                            <div className={style.desc} key={describe.key}>
+                                <h3>{describe.title}</h3>
+                                <p>{describe.value}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
