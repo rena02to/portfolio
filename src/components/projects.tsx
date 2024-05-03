adb9d743559191685e33fca0bbc1a1776d9d5abd
Renato Alves <rena02to@gmail.com>
2024-05-02 23:46:38 -0300

    adicionando o arquivo de projects

---
+++ src/components/projects.tsx
@@ -0,0 +1,7 @@
+import style from '@/styles/projects.module.scss';
+
+export default function Projects(){
+    return(
+        <section id="projects" className={style.projects}></section>
+    );
+}
