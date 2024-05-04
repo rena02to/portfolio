3c6e8193fa016a85f0bcd0f8ed7cb1f18f5f2dc6
Renato Alves <rena02to@gmail.com>
2024-05-03 22:15:27 -0300

    adicionando a estrutura básica para projetos

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,26 @@
 import style from '@/styles/projects.module.scss';
 
 export default function Projects(){
+    const projects = [
+        { 
+            key: null,
+            name: null,
+            desc: null,
+            stack: [{key: null, name: null}],
+            github: null,
+            link: null,
+            image: null,
+            status: null,
+            pinned: null
+        }
+    ]
+
     return(
-        <section id="projects" className={style.projects}></section>
+        <section id="projects" className={style.projects}>
+            {projects.map((project) => (
+                <div className={style.cardproject} key={project.key}>
+                </div>
+            ))}
+        </section>
     );
 }
