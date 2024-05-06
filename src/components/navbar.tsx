b36f20792d60134b1024c54af1ec0803244e22b0
Renato Alves <rena02to@gmail.com>
2024-05-05 23:30:55 -0300

    correção no commit anterior

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -83,7 +83,7 @@ export default function Navbar(){
 
     return(
         <>
-            {!menuCompress ?
+            {menuCompress ?
                 <>
                     <nav className={style.navcompress}>
                         {menulinks.map((menulink) => (
