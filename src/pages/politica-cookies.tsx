53ac9ebe7a2f2b0b48956f151dc96fae73b2928d
Renato Alves <rena02to@gmail.com>
2024-05-14 19:25:58 -0300

    inserindo a mensagem em pt

--- src/pages/politica-cookies.tsx
+++ src/pages/politica-cookies.tsx
@@ -1,22 +1,25 @@
 import style from '@/styles/policy.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
 import { LiaCookieBiteSolid } from 'react-icons/lia';
 import { TbArrowBackUpDouble } from 'react-icons/tb';
 
 export default function Policy(){
+    const { t } = useTranslation();
+
     return(
         <div className={style.cookiespolicy}>
-            <Link href='/'><TbArrowBackUpDouble/><p>Voltar para o portfolio</p></Link>
-            <Image src='/images/cookies.png' alt='Imagem de cookies' width={100} height={100}/>
+            <Link href='/'><TbArrowBackUpDouble/><p>{t('policy.link')}</p></Link>
+            <Image src='/images/cookies.png' alt='Policy Cookies Image' width={100} height={100}/>
             <div className={style.intern}>
-                <p>Bem-vindo à nossa política de cookies! Aqui, queremos informá-lo sobre como usamos cookies em nosso site para melhorar sua experiência de navegação.</p>
-                <h3><LiaCookieBiteSolid/>O que são cookies?<LiaCookieBiteSolid/></h3>
-                <p>Cookies são pequenos arquivos de texto armazenados no seu navegador quando você visita nosso site, e eles têm como objetivo melhorar sua experiência de navegação.</p>
-                <h3><LiaCookieBiteSolid/>Cookies essenciais<LiaCookieBiteSolid/></h3>
-                <p>Os cookies essenciais são fundamentais para o funcionamento do nosso site. Eles são utilizados apenas para lembrar se você aceitou ou negou o uso dos cookies opcionais. Esses cookies não coletam informações pessoais e não podem ser desativados.</p>
-                <h3><LiaCookieBiteSolid/>Cookies opcionais<LiaCookieBiteSolid/></h3>
-                <p>Os cookies opcionais nos ajudam a melhorar sua experiência no site ao lembrar suas preferências, como o idioma escolhido. Eles não são essenciais, mas permitem personalizar o conteúdo para atender às suas preferências. Você pode escolher permitir ou recusar esses cookies.</p>
+                <p>{t('policy.welcome')}</p>
+                <h3><LiaCookieBiteSolid/>{t('policy.what')}<LiaCookieBiteSolid/></h3>
+                <p>{t('policy.responsewhat')}</p>
+                <h3><LiaCookieBiteSolid/>{t('policy.essentials')}<LiaCookieBiteSolid/></h3>
+                <p>{t('policy.responseessentials')}</p>
+                <h3><LiaCookieBiteSolid/>{t('policy.optionals')}<LiaCookieBiteSolid/></h3>
+                <p>{t('policy.responseoptionals')}</p>
             </div>
         </div>
     );
