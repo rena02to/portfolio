2b935454de8dd589004b3a99cc82e63b7ef9a1ab
Renato Alves <rena02to@gmail.com>
2024-05-14 20:47:54 -0300

    inserindo a estrutura básica da pagina de configurações

--- src/pages/configuracoes.tsx
+++ src/pages/configuracoes.tsx
@@ -1,9 +1,17 @@
 import style from '@/styles/settings.module.scss';
+import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
+import { TbArrowBackUpDouble } from 'react-icons/tb';
 
-export default function Settings(){
+export default function Settings()
+{
+    const { t } = useTranslation();
+
     return(
         <div className={style.settings}>
-            <p>Em breve, aqui ficarão as configurações</p>
+            <Link href='/' className={style.back}><TbArrowBackUpDouble/><p>{t('policy.link')}</p></Link>
+            <h3>{t('settings.title')}</h3>
+            <div className={style.control}></div>
         </div>
     )
 }
