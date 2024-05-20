65df8d11899f3fbc1805375d01d7311a2e75fb82
Renato Alves <rena02to@gmail.com>
2024-05-20 00:11:59 -0300

    inserindo a estrutura básica do formulário de contato

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,9 +1,47 @@
 import style from '@/styles/contact.module.scss'; 
+import { ErrorMessage, Form, Formik } from 'formik';
+import { useState } from 'react';
 
 export default function Contact(){
+    const initialValues = {};
+    const [ isSubmitting, setIsSubmitting ] = useState(false);
+
+    const handleSubmit = () => {}
+
     return(
         <section className={style.contact} id='contact'>
             <h2>Contate-me</h2>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                {({ isSubmitting }) => (
+                    <Form>
+                        <div className={style.inputbox}>
+                            <label htmlFor="name">Seu nome e sobrenome<span>*</span></label>
+                            <input type="text" id='name'/>
+                            <ErrorMessage name='name'/>
+                        </div>
+
+                        <div className={style.inputbox}>
+                            <label htmlFor="email">Seu e-mail<span>*</span></label>
+                            <input type="email" name="email" id="email"/>
+                            <ErrorMessage name='email'/>
+                        </div>
+
+                        <div className={style.inputbox}>
+                            <label htmlFor="subject">Assunto da mensagem<span>*</span></label>
+                            <input type="text" id='subject'/>
+                            <ErrorMessage name='subject'/>
+                        </div>
+
+                        <div className={style.inputbox}>
+                            <label htmlFor="message">Mensagem<span>*</span></label>
+                            <textarea name="message" id="message"/>
+                            <ErrorMessage name='message'/>
+                        </div>
+
+                        <button type="submit" disabled={!isSubmitting}>Enviar</button>
+                    </Form>
+                )}
+            </Formik>
         </section>
     );
 }
