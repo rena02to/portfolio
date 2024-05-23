22b2d5aec22345143351a7998832a8d21dbd34a9
Renato Alves <rena02to@gmail.com>
2024-05-23 00:34:08 -0300

    alterações feitas até o presente momento

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,10 +1,11 @@
 import style from '@/styles/contact.module.scss'; 
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { useState } from 'react';
-import { BsFillSendFill } from 'react-icons/bs';
+import { BsTrash3Fill } from 'react-icons/bs';
 import { FaFileCircleQuestion, FaUserTie } from 'react-icons/fa6';
-import { IoClose, IoMail } from 'react-icons/io5';
+import { IoMail } from 'react-icons/io5';
 import { RiQuestionAnswerFill } from 'react-icons/ri';
+import { TiUpload } from 'react-icons/ti';
 
 export default function Contact(){
     const initialValues = {
@@ -12,9 +13,38 @@ export default function Contact(){
         email: '',
         subject: '',
         message: '',
+        yesorno: 'no',
+        file: '',
     };
     const [ isSubmitting, setIsSubmitting ] = useState(false);
     const [ isClearing, setIsClearing ] = useState(false);
+    const [ attach, setAttach ] = useState(false);
+    const [ file, setFile ] = useState<File | null>(null);
+    const [ attached, setAttached ] = useState(false);
+
+    const handleUpload = (event: any) => {
+        let fileinput =  event.target.files[0];
+        if(fileinput){
+            setFile(fileinput);
+            setAttached(true);
+        }
+    }
+    
+    const handleDeleteFile = () => {
+        const inputFile = document.getElementById('file') as HTMLInputElement;
+        if(inputFile){
+            inputFile.value = '';
+        }
+        setFile(null);
+        setAttached(false);
+    }
+
+    const handleReset = (resetForm : any) => {
+        resetForm();
+        setFile(null);
+        setAttached(false);
+        setAttach(false);
+    }
 
     const handleSubmit = () => {}
 
@@ -22,7 +52,7 @@ export default function Contact(){
         <section className={style.contact} id='contact'>
             <h2>Contate-me</h2>
             <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-                {({}) => (
+                {({ resetForm }) => (
                     <Form>
                         <div className={style.title}>
                             <h3>Formulário de contato</h3>
@@ -33,7 +63,7 @@ export default function Contact(){
                             <label htmlFor="name">Nome e sobrenome <span>*</span></label>
                             <div className={style.input}>
                                 <FaUserTie/>
-                                <Field type="text" name="name" id='name' placeholder='Digite aqui seu nome e sobrenome'/>
+                                <Field type="text" name="name" id='name' placeholder='Digite aqui seu nome e sobrenome' required/>
                             </div>
                             <ErrorMessage name='name'/>
                         </div>
@@ -42,7 +72,7 @@ export default function Contact(){
                             <label htmlFor="email">E-mail<span>*</span></label>
                             <div className={style.input}>
                                 <IoMail/>
-                                <Field type="email" name="email" id="email" placeholder='Digite aqui seu e-mail'/>
+                                <Field type="email" name="email" id="email" placeholder='Digite aqui seu e-mail' required/>
                             </div>
                             <ErrorMessage name='email'/>
                         </div>
@@ -51,7 +81,7 @@ export default function Contact(){
                             <label htmlFor="subject">Assunto<span>*</span></label>
                             <div className={style.input}>
                                 <FaFileCircleQuestion/>
-                                <Field type="text" name="subject" id='subject' placeholder='Digite aqui o assunto'/>
+                                <Field type="text" name="subject" id='subject' placeholder='Digite aqui o assunto' required/>
                             </div>
                             <ErrorMessage name='subject'/>
                         </div>
@@ -60,14 +90,43 @@ export default function Contact(){
                             <label htmlFor="message">Mensagem<span>*</span></label>
                             <div className={style.input}>
                                 <RiQuestionAnswerFill/>
-                                <Field as="textarea" name="message" id="message" placeholder='Digite aqui a mensagem'/>
+                                <Field as="textarea" name="message" id="message" placeholder='Digite aqui a mensagem' required/>
                             </div>
                             <ErrorMessage name='message'/>
                         </div>
 
+                        <div className={style.inputbox}>
+                            <label htmlFor="file">Gostaria de anexar algum arquivo?</label>
+                            <div className={style.inputradio}>
+                                <label className={style.radio}>
+                                    <Field type="radio" name="yesorno" value="yes" onClick={() => setAttach(true)}/>
+                                    <p>Sim</p>
+                                </label>
+                                <label className={style.radio}>
+                                    <Field type="radio" name="yesorno" value="no" onClick={() => setAttach(false)}/>
+                                    <p>Não</p>
+                                </label>
+                            </div>
+                            {attach ? 
+                                <div className={style.box}>
+                                    <div className={style.inputboxfile}>
+                                        <Field className={style.inputfile} type="file" name="file" id="file" accept=".pdf, .jpg, .jpeg, .txt" onChange={handleUpload} required={attach}/>
+                                        <label htmlFor='file' className={!attached ? style.pseudoinput : style.pseudoinputup}>
+                                            <p>{file === null ? "Encontrar no dispositivo" : file.name}</p>
+                                            <TiUpload/>
+                                        </label>
+                                    </div>
+                                    <button type="button" title="Remover arquivo" disabled={attached ? false : true} onClick={handleDeleteFile}><BsTrash3Fill/></button>
+                                </div>
+                                :
+                                null
+                            }
+                            <ErrorMessage name='file'/>
+                        </div>
+
                         <div className={style.buttons}>
-                            <button type="submit" className={style.submit} disabled={!isSubmitting}>Enviar</button>
-                            <button type="reset" className={style.reset} disabled={!isClearing}>Limpar</button>
+                            <button type="submit" className={style.submit} disabled={isSubmitting}>Enviar</button>
+                            <button type="reset" className={style.reset} disabled={isClearing} onClick={() => handleReset(resetForm)}>Limpar</button>
                         </div>
                     </Form>
                 )}
