18778523a419253d15b0057b9ee9f6594dc4acaf
rena02to <rena02to@gmail.com>
2024-06-19 18:27:35 -0300

    removendo o campode anexo de arquivo

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -11,52 +11,21 @@ import { TiUpload } from 'react-icons/ti';
 export default function Contact(){
     const [ isSubmitting, setIsSubmitting ] = useState(false);
     const [ isClearing, setIsClearing ] = useState(false);
-    const [ attach, setAttach ] = useState(false);
-    const [ file, setFile ] = useState<File | null>(null);
-    const [ attached, setAttached ] = useState(false);
     const validationSchema = Yup.object().shape({
         name: Yup.string().required('Campo obrigatório'),
         email: Yup.string().email('E-mail inválido').required('Campo obrigatório'),
         subject: Yup.string().required('Campo obrigatório'),
         message: Yup.string().required('Campo obrigatório'),
-        yesorno: Yup.string().required('Campo obrigatório'),
-        // file: Yup.mixed().when('yesorno', {
-        //     is: 'yes',
-        //     then: Yup.mixed().required('Arquivo é obrigatório'),
-        //     otherwise: Yup.mixed().notRequired(),
-        // }),
     });
     const initialValues = {
         name: '',
         email: '',
         subject: '',
         message: '',
-        yesorno: 'no',
-        file: '',
     };
-
-    const handleUpload = (event: any) => {
-        let fileinput =  event.target.files[0];
-        if(fileinput){
-            setFile(fileinput);
-            setAttached(true);
-        }
-    }
-    
-    const handleDeleteFile = () => {
-        const inputFile = document.getElementById('file') as HTMLInputElement;
-        if(inputFile){
-            inputFile.value = '';
-        }
-        setFile(null);
-        setAttached(false);
-    }
 
     const handleReset = (resetForm : any) => {
         resetForm();
-        setFile(null);
-        setAttached(false);
-        setAttach(false);
     }
 
     const handleSubmit = () => {}
@@ -78,7 +47,7 @@ export default function Contact(){
                                 <FaUserTie/>
                                 <Field type="text" name="name" id='name' placeholder='Digite aqui seu nome e sobrenome' required/>
                             </div>
-                            <ErrorMessage className={style.error} name='name'/>
+                            <ErrorMessage className={style.error} name='name' component="p"/>
                         </div>
 
                         <div className={style.inputbox}>
@@ -87,7 +56,7 @@ export default function Contact(){
                                 <IoMail/>
                                 <Field type="email" name="email" id="email" placeholder='Digite aqui seu e-mail' required/>
                             </div>
-                            <ErrorMessage className={style.error} name='email'/>
+                            <ErrorMessage className={style.error} name='email' component="p"/>
                         </div>
 
                         <div className={style.inputbox}>
@@ -96,7 +65,7 @@ export default function Contact(){
                                 <FaFileCircleQuestion/>
                                 <Field type="text" name="subject" id='subject' placeholder='Digite aqui o assunto' required/>
                             </div>
-                            <ErrorMessage className={style.error} name='subject'/>
+                            <ErrorMessage className={style.error} name='subject' component="p"/>
                         </div>
 
                         <div className={style.inputbox}>
@@ -105,39 +74,7 @@ export default function Contact(){
                                 <RiQuestionAnswerFill/>
                                 <Field as="textarea" name="message" id="message" placeholder='Digite aqui a mensagem' required/>
                             </div>
-                            <ErrorMessage className={style.error} name='message'/>
-                        </div>
-
-                        <div className={style.inputbox}>
-                            <label htmlFor="file">Gostaria de anexar algum arquivo?</label>
-                            <div className={style.inputradio}>
-                                <label className={style.radio}>
-                                    <Field type="radio" name="yesorno" value="yes" onClick={() => setAttach(true)}/>
-                                    <p>Sim</p>
-                                </label>
-                                <label className={style.radio}>
-                                    <Field type="radio" name="yesorno" value="no" onClick={() => setAttach(false)}/>
-                                    <p>Não</p>
-                                </label>
-                            </div>
-                            {attach ? 
-                                <>
-                                    <div className={style.box}>
-                                        <div className={style.inputboxfile}>
-                                            <Field className={style.inputfile} type="file" name="file" id="file" accept=".pdf, .jpg, .jpeg, .txt" onChange={handleUpload} required={attach}/>
-                                            <label htmlFor='file' className={!attached ? style.pseudoinput : style.pseudoinputup}>
-                                                <p>{file === null ? "Encontrar no dispositivo" : file.name}</p>
-                                                <TiUpload/>
-                                            </label>
-                                        </div>
-                                        <button type="button" title="Remover arquivo" disabled={attached ? false : true} onClick={handleDeleteFile}><BsTrash3Fill/></button>
-                                    </div>
-                                    <ErrorMessage className={style.error} name='file'/>
-                                </>
-                                :
-                                null
-                            }
-                            <ErrorMessage className={style.error} name='yesorno'/>
+                            <ErrorMessage className={style.error} name='message' component="p"/>
                         </div>
 
                         <div className={style.buttons}>
